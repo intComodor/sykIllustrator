1ab6861da413b9a2107a9e13e48ee7e7db37f705
stephen foerster <stephen.foerster1@gmail.com>
2023-01-14 01:32:30 +0100

    #6 fix canvas ui issue ajout du canvas, essaie pr dynamiser la taille par rapport à l'écran

--- src/app/components/drawing-board/drawing-board.component.ts
+++ src/app/components/drawing-board/drawing-board.component.ts
@@ -9,18 +9,46 @@ export class DrawingBoardComponent implements OnInit {
 
   constructor() { }
 
-  click: boolean = false;
+  isDrawing: boolean = false;
 
   ngOnInit(): void {
+    let canvas = document.getElementById("canvas") as HTMLCanvasElement;
+    canvas.width = 900;
+    canvas.height = 600;
+
   }
 
-  onclick() {
-    if (this.click) {
-      this.click = false;
-    }
-    else {
-      this.click = true;
-    }
+  startDrawing(event: MouseEvent) {
+    let canvas = document.getElementById("canvas") as HTMLCanvasElement;
+    let rect = canvas.getBoundingClientRect();
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top;
+    let ctx = canvas.getContext("2d");
+    if (!ctx) { return; }
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    this.isDrawing = true;
   }
+  
+  
+  drawing(event: MouseEvent) {
+    console.log("drawing " + event.offsetX + " " + event.offsetY);
+    if (!this.isDrawing) { return; }
+    let canvas = document.getElementById("canvas") as HTMLCanvasElement;
+    let ctx = canvas.getContext("2d");
+    if (!ctx) { return; }
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 2;
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+  }
+  
+  stopDrawing() {
+    this.isDrawing = false;
+  }
+
+
+  
+  
 
 }
