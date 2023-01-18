3718a13613e428482f09ab3964858d6b22c51880
Karim Bouali <comodor@MacBook-Pro-de-Karim.local>
2023-01-18 23:27:50 +0100

    fix le fait que onChangeFormat soit appelé au chargement de la page

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable()
 export class DataService {
 
-  private sendFormat = new BehaviorSubject('');
+  private sendFormat = new Subject();
   listenFormat = this.sendFormat.asObservable();
 
   constructor() { }
