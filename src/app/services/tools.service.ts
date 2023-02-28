2781791aa22ec7097c84dacdb02e00cc5be0b9d6
Youness BASRI <youness.basri@etu.unistra.fr>
2023-03-01 00:51:46 +0100

    segment fonctionnels

--- src/app/services/tools.service.ts
+++ src/app/services/tools.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { Tool } from 'src/app/types/tool';
 import { Pencil } from 'src/app/types/pencil';
 import { RectForm } from 'src/app/types/rect-form';
+import { Line } from '../types/line';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ToolsService {
-  tools: Tool[] = [new Pencil(), new RectForm()];
+  tools: Tool[] = [new Line(), new Pencil(), new RectForm()];
   indexTool = 0;
   currentTool: Tool = this.tools[this.indexTool];
 
