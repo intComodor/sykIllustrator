cb406bdd29a88290b6f99b2f7a0de3cc6e6c0e9d
Youness BASRI <youness.basri@etu.unistra.fr>
2023-04-08 02:41:57 +0200

    add eraser tool

---
+++ src/app/types/eraser.ts
@@ -0,0 +1,22 @@
+import { CouplePoints } from './couplePoints';
+import { Pencil } from './pencil';
+import { Tool } from './tool';
+
+export class Eraser extends Pencil {
+  constructor() {
+    super();
+    this.name = 'Eraser';
+    this.icon = 'eraser.svg';
+  }
+
+  override draw(coords: CouplePoints): void {
+    const ctx = this.canvasService.canvasCtx;
+    this.canvasService.canvasCtx.strokeStyle = '#ffffff';
+    this.canvasService.canvasCtx.lineWidth =
+      this.drawingDataService.getLineWidth();
+    ctx.beginPath();
+    ctx.moveTo(coords.startX, coords.startY);
+    ctx.lineTo(coords.endX, coords.endY);
+    ctx.stroke();
+  }
+}
