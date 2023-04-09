cf2ff384bd94075ab8fc440ed591baafc1f95c7e
Youness BASRI <youness.basri@etu.unistra.fr>
2023-04-09 17:43:29 +0200

    add triangle shape

---
+++ src/app/types/triangle.ts
@@ -0,0 +1,83 @@
+import { CouplePoints } from './couplePoints';
+import { Tool } from './tool';
+
+export class Triangle extends Tool {
+  constructor() {
+    super('Triangle', 'black');
+  }
+
+  /** Coordinates of the rectangle to draw */
+  coords: CouplePoints = new CouplePoints(0, 0, 0, 0);
+  lastState!: HTMLCanvasElement;
+  isDrawing = false;
+
+  initTool(): void {
+    // behavior when the mouse is pressed
+    this.eventsSubscription.push(
+      this.mouseEventService
+        .mousedown$(this.canvasService.canvas)
+        .subscribe(event => {
+          this.coords.startX = event.offsetX;
+          this.coords.startY = event.offsetY;
+
+          this.lastState = this.canvasService.createTmpCanvas();
+          this.isDrawing = true;
+        })
+    );
+
+    // behavior when the mouse is moved
+    this.eventsSubscription.push(
+      this.mouseEventService.mousemove$(window).subscribe(event => {
+        if (!this.isDrawing) return;
+
+        if (this.canvasService.isInCanvas(event.clientX, event.clientY)) {
+          this.canvasService.drawCanvas(this.lastState);
+          this.coords.endX = event.offsetX;
+          this.coords.endY = event.offsetY;
+          this.draw(this.coords);
+        } else {
+          this.isDrawing = false;
+          this.drawingDataService.pushState();
+          this.lastState = this.canvasService.createTmpCanvas();
+        }
+      })
+    );
+
+    // behavior when the mouse is released
+    this.eventsSubscription.push(
+      this.mouseEventService.mouseup$.subscribe(event => {
+        if (!this.isDrawing) return;
+
+        this.coords.endX = event.offsetX;
+        this.coords.endY = event.offsetY;
+
+        // Check if the mouse is still on the canvas before drawing
+        if (this.canvasService.isInCanvas(event.clientX, event.clientY)) {
+          this.draw(this.coords);
+          this.drawingDataService.pushState();
+          this.isDrawing = false;
+          this.lastState = this.canvasService.createTmpCanvas();
+        }
+      })
+    );
+  }
+
+  draw(coords: CouplePoints): void {
+    // Calculate the coordinates of the rectangle to draw
+    const ctx = this.canvasService.canvasCtx;
+
+    this.canvasService.canvasCtx.strokeStyle =
+      this.drawingDataService.getColor();
+    this.canvasService.canvasCtx.fillStyle = this.drawingDataService.getColor();
+    this.canvasService.canvasCtx.lineWidth =
+      this.drawingDataService.getLineWidth();
+
+    ctx.beginPath();
+    ctx.moveTo(coords.startX, coords.startY);
+    ctx.lineTo(coords.endX, coords.endY);
+    ctx.lineTo(coords.startX - (coords.endX - coords.startX), coords.endY);
+    ctx.closePath();
+    if (this.drawingDataService.isFill()) ctx.fill(); // Remplir le triangle
+    else ctx.stroke();
+  }
+}
